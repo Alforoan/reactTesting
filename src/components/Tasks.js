32993d9096c540263be76fff78c42d16c9c0c4fb
Jay <jdragon2050@gmail.com>
2022-12-28 20:51:57 -0800

    add handleclick for message variable for placeholder so when user types it changes

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { FaHome } from "react-icons/fa";
 
-const handleClick = () => {};
-
 function Tasks() {
-  const [message, setMessage] = React.useState("");
+  const [message, setMessage] = React.useState("Add a task");
+  const handleClick = () => {
+    setMessage("Type something you have to do!");
+  };
+
   return (
     <div>
       <h1>
         <FaHome /> Tasks
       </h1>
       <form>
-        <input onClick={handleClick} type="text" placeholder={message} />
+        <input
+          onClick={() => handleClick()}
+          type="text"
+          placeholder={message}
+        />
       </form>
     </div>
   );
