e7ad29a877b7d64d3a12556fa06bc73048f4c04a
Jay <jdragon2050@gmail.com>
2022-12-28 19:58:01 -0800

    change button to form and add input with a placeholder

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -2,14 +2,14 @@ import React from "react";
 import { FaHome } from "react-icons/fa";
 
 function Tasks() {
-  const [message, setMessage] = React.useState("");
-
   return (
     <div>
       <h1>
         <FaHome /> Tasks
       </h1>
-      <button className="task-btn"></button>
+      <form>
+        <input type="text" placeholder="Add a task" />
+      </form>
     </div>
   );
 }
