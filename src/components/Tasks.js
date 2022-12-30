d3120e42a72053ca97e438af3a478f683c3b7a73
Jay <jdragon2050@gmail.com>
2022-12-29 17:29:09 -0800

    change placeholder from variable to just a string

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,22 +3,27 @@ import { FaHome } from "react-icons/fa";
 
 function Tasks() {
   const [message, setMessage] = React.useState("Add a task");
+  const [task, setTask] = React.useState("");
   const handleClick = () => {
     setMessage("Type something you have to do!");
   };
+
+  const handleSubmit = () => {};
 
   return (
     <div>
       <h1>
         <FaHome /> Tasks
       </h1>
-      <form>
+      <div>{task}</div>
+      <form onSubmit={() => handleSubmit()} type="submit">
         <input
           className="task-form"
           onClick={() => handleClick()}
           type="text"
-          placeholder={message}
+          placeholder="Add a task"
         />
+        <button type="submit"></button>
       </form>
     </div>
   );
