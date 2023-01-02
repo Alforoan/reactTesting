b7a8dab3521940ad53449baed3de96cd02b7c011
Jay <jdragon2050@gmail.com>
2023-01-01 23:48:47 -0800

    add functionality that removes the completed tasks from the original list

--- src/components/List.js
+++ src/components/List.js
@@ -5,34 +5,36 @@ import { FaTrash, FaRegStar, FaStar } from "react-icons/fa";
 const List = ({ tasks, removeTask, editTask, importantTask, completeTask }) => {
   return (
     <div>
-      {tasks.map((task) => {
-        const { id, title, isImportant, isCompleted } = task;
-        return (
-          <article key={id}>
-            <p
-              onClick={() => completeTask(id)}
-              className={isCompleted ? "title-completed" : "title"}
-            >
-              {title}
-            </p>
-            <div className="btn-container">
-              <button className="edit" onClick={() => editTask(id)}>
-                <FaEdit className="edit-icon" />
-              </button>
-              <button className="delete" onClick={() => removeTask(id)}>
-                <FaTrash className="delete-icon" />
-              </button>
-              <button className="star" onClick={() => importantTask(id)}>
-                {isImportant ? (
-                  <FaStar className="star-icon" />
-                ) : (
-                  <FaRegStar className="star-icon" />
-                )}
-              </button>
-            </div>
-          </article>
-        );
-      })}
+      {tasks
+        .filter((task) => task.isCompleted !== true)
+        .map((task) => {
+          const { id, title, isImportant, isCompleted } = task;
+          return (
+            <article key={id}>
+              <p
+                onClick={() => completeTask(id)}
+                className={isCompleted ? "title-completed" : "title"}
+              >
+                {title}
+              </p>
+              <div className="btn-container">
+                <button className="edit" onClick={() => editTask(id)}>
+                  <FaEdit className="edit-icon" />
+                </button>
+                <button className="delete" onClick={() => removeTask(id)}>
+                  <FaTrash className="delete-icon" />
+                </button>
+                <button className="star" onClick={() => importantTask(id)}>
+                  {isImportant ? (
+                    <FaStar className="star-icon" />
+                  ) : (
+                    <FaRegStar className="star-icon" />
+                  )}
+                </button>
+              </div>
+            </article>
+          );
+        })}
     </div>
   );
 };
